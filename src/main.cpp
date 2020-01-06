1f66c5bf297c11c3c6fbbcc9f9f57bc5098e712e
Richard Lyon <richardlyon@fastmail.com>
2020-01-06 12:34:29 +0000

    Refactor code

--- src/main.cpp
+++ src/main.cpp
@@ -12,61 +12,40 @@
 #define LIGHT_SENSOR_PIN 5
 #define LED_PIN 16
 
-bool oldValue, newValue;
+bool oldValue;
 
 static void process_boiler_state_cb(void *arg);
 
 enum mgos_app_init_result mgos_app_init(void)
 {
-
   mgos_gpio_setup_output(LED_PIN, 0);
   mgos_gpio_set_mode(LIGHT_SENSOR_PIN, MGOS_GPIO_MODE_INPUT);
 
   long update_interval_ms = mgos_sys_config_get_sensor_interval();
 
-  newValue = mgos_gpio_read(LIGHT_SENSOR_PIN);
-  oldValue = !newValue;
+  oldValue = !mgos_gpio_read(LIGHT_SENSOR_PIN);
 
   mgos_set_timer(update_interval_ms /* ms */, MGOS_TIMER_REPEAT, process_boiler_state_cb, NULL);
   return MGOS_APP_INIT_SUCCESS;
 }
 
-static void process_boiler_state_cb(void *arg)
+static void process_boiler_state_cb(void *)
 {
-  newValue = mgos_gpio_read(LIGHT_SENSOR_PIN);
+  bool newValue = mgos_gpio_read(LIGHT_SENSOR_PIN);
+  const char *state = newValue ? "ON" : "OFF";
 
-  /** Send the state if configuration is true */
-  bool sendState = (mgos_sys_config_get_sensor_sendstate());
-  if (sendState)
+  /** always send the state if configuration is true */
+  if (mgos_sys_config_get_sensor_sendstate())
   {
-    if (newValue == true)
-    {
-      mgos_mqtt_pub("/boiler/state", "ON", 2, 1, 0);
-    }
-    else
-    {
-      mgos_mqtt_pub("/boiler/state", "OFF", 3, 1, 0);
-    }
+    mgos_mqtt_pub("/boiler/state", state, strlen(state), 1, 0);
   }
 
-  /** Send the change if it has changed */
+  /** only send the state if it has changed */
   if (newValue != oldValue)
   {
+    LOG(LL_INFO, (state));
+    mgos_mqtt_pub("/boiler/change", state, strlen(state), 1, 0);
+    mgos_gpio_write(LED_PIN, !newValue); // inverted; = "ON"
     oldValue = newValue;
-    if (newValue == true)
-    {
-      LOG(LL_INFO, ("ON"));
-      mgos_mqtt_pub("/boiler/change", "ON", 2, 1, 0);
-      mgos_gpio_write(LED_PIN, false); // inverted; = "ON"
-    }
-    else
-    {
-      LOG(LL_INFO, ("OFF"));
-      mgos_mqtt_pub("/boiler/change", "OFF", 3, 1, 0);
-      mgos_gpio_write(LED_PIN, true); // inverted; = "OFF"
-    }
   }
-
-  // mgos_gpio_toggle(LED_PIN);
-  (void)arg;
 }
